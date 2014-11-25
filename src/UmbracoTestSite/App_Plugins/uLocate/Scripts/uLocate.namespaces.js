d7356b0e0163ea53766898af1f2aa47717aa0125
Kyle Weems <kyle@mindfly.com>
2014-11-24 16:47:01 -0800

    Added first uLocate controller.

---
+++ src/UmbracoTestSite/App_Plugins/uLocate/Scripts/uLocate.namespaces.js
@@ -0,0 +1,8 @@
+﻿(function(uLocate, undefined) {
+
+    uLocate.Controllers = {};
+    uLocate.Directives = {};
+    uLocate.Models = {};
+    uLocate.Services = {};
+
+}(window.uLocate = window.uLocate || {}));
