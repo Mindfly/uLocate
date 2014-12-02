e0fd1f045b28cef5752e33240e1cdcab464ba750
Kyle Weems <kyle@mindfly.com>
2014-12-02 10:03:52 -0800

    .

--- src/UmbracoTestSite/App_Plugins/uLocate/Scripts/Controllers/locations.controller.js
+++ src/UmbracoTestSite/App_Plugins/uLocate/Scripts/Controllers/locations.controller.js
@@ -172,6 +172,7 @@
 
         $scope.processEditDialog = function(data) {
             console.info(data);
+            notificationsService.success("Location edited", "This location has been successfully updated. #h5yr!");
         };
 
         /*-------------------------------------------------------------------*/
