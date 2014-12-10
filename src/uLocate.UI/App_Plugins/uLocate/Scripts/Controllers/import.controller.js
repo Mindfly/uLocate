a456366edbc3aaab515e62e94392620cc151c54c
Kyle Weems <kyle@mindfly.com>
2014-12-10 21:11:09 +0000

    Work on import using code example from Shannon. Needs server side to prove functionality.

--- src/uLocate.UI/App_Plugins/uLocate/Scripts/Controllers/import.controller.js
+++ src/uLocate.UI/App_Plugins/uLocate/Scripts/Controllers/import.controller.js
@@ -1,6 +1,6 @@
 ﻿(function (controllers, undefined) {
 
-    controllers.ImportController = function ($scope, $routeParams, treeService, assetsService, dialogService, navigationService, notificationsService) {
+    controllers.ImportController = function ($scope, $routeParams, $http, treeService, assetsService, dialogService, navigationService, notificationsService) {
 
         /*-------------------------------------------------------------------
          * Initialization Methods
@@ -14,7 +14,7 @@
          * @description - Called when the $scope is initalized.
          */
         $scope.init = function () {
-            $scope.selectedView = $routeParams.id;
+            $scope.setVariables();
         };
 
         /**
@@ -26,14 +26,41 @@
          */
         $scope.setVariables = function () {
             $scope.selectedView = $routeParams.id;
-            $scope.currentNode = false;
-            $scope.openMenu = false;
-            $scope.getCurrentNode();
+            $scope.dataType = 'Default';
+            $scope.file = false;
         };
 
         /*-------------------------------------------------------------------
          * Event Handler Methods
          *-------------------------------------------------------------------*/
+
+        $scope.fileSelected = function (files) {
+            if (files.length > 0) {
+                $scope.file = files[0];
+            }
+        };
+
+
+        $scope.uploadFile = function () {
+            $http({
+                method: 'POST',
+                url: "/Api/PostStuff",
+                // TODO: Change Content-Type to undefined in Umbraco 7.5 (or whenever the Angular version is bumped to 1.2 or higher)
+                headers: { 'Content-Type': false },
+                transformRequest: function (data) {
+                    var formData = new FormData();
+                    formData.append("dataType", angular.toJson(data.dataType));
+                    formData.append("file", data.file);
+                    return formData;
+                },
+                //Create an object that contains the model and files which will be transformed in the above transformRequest method
+                data: { dataType: $scope.dataType, file: $scope.file }
+            }).success(function (data, status, headers, config) {
+                notificationsService.success("File imported.");
+            }).error(function (data, status, headers, config) {
+                notificationsService.error("File import failed.");
+            });
+        };
 
         /*-------------------------------------------------------------------
          * Helper Methods
@@ -47,6 +74,6 @@
 
     app.requires.push('angularFileUpload');
 
-    angular.module('umbraco').controller('uLocate.Controllers.ImportController', ['$scope', '$routeParams', 'treeService', 'assetsService', 'dialogService', 'navigationService', 'notificationsService', uLocate.Controllers.ImportController]);
+    angular.module('umbraco').controller('uLocate.Controllers.ImportController', ['$scope', '$routeParams', '$http', 'treeService', 'assetsService', 'dialogService', 'navigationService', 'notificationsService', uLocate.Controllers.ImportController]);
 
 }(window.uLocate.Controllers = window.uLocate.Controllers || {}));
