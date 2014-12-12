d71ecf1eb09f19be99a397a434776d5e4001b2a9
Janae Cram <janae@mindfly.com>
2014-12-11 16:40:11 -0800

    Localization

--- src/uLocate.UI/App_Plugins/uLocate/Langs/en-us.js
+++ src/uLocate.UI/App_Plugins/uLocate/Langs/en-us.js
@@ -2,5 +2,18 @@
     "locations" : "Locations",
     "address" : "Address",
     "phone" : "Phone",
-    "latLong" : "Lat/Long"
+    "latLong" : "Lat/Long",
+    "streetAddress" : "Street Address",
+    "streetAddressHelp" : "Street address or c/o",
+    "extenedAddress" : "Extended Address",
+    "extendedAddressHelp" : "Suite, apartment, unit, etc",
+    "city" : "City",
+    "stateProvince" : "State/Province",
+    "zipPostalCode" : "Zip/Postal Code",
+    "country" : "Country",
+    "latitude" : "Latitude",
+    "latitudeHelp" : "Optional. Will auto-generate based on address unless both latitude and longitude are manually entered.",
+    "longitude" : "Longitude",
+    "longitudeHelp" : "Optional. Will auto-generate based on address unless both latitude and longitude are manually entered.",
+    "locationName" : "Enter a name for your location"
 }
