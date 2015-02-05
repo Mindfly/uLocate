f97ad3974e979f5aed25c3091f74ea938f868bdd
Heather Floyd <hlf@floydinnovations.com>
2015-02-05 18:07:16 -0500

    adding CmsDataType class

---
+++ src/uLocate/Models/CmsDataType.cs
@@ -0,0 +1,60 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace uLocate.Models
+{
+    using umbraco.presentation.webservices;
+
+    public class CmsDataType
+    {
+        public enum DbType
+        {
+            Ntext,
+            Nvarchar,
+            Integer,
+            Date,
+            Unknown
+        }
+
+        public int Key { get; set; }
+
+        public int DataTypeId { get; set; }
+
+        public string PropertyEditorAlias { get; set; }
+
+        public string DatabaseTypeString { get; set; }
+
+        public DbType DatabaseType
+        {
+            get
+            {
+               return StringToDbType(this.DatabaseTypeString);
+            }
+        }
+
+        private DbType StringToDbType(string DatabaseTypeText)
+        {
+            switch (DatabaseTypeText)
+            {
+                case Constants.DbNtext:
+                    return DbType.Ntext;
+                    break;
+                case Constants.DbNvarchar:
+                    return DbType.Nvarchar;
+                    break;
+                case Constants.DbInteger:
+                    return DbType.Integer;
+                    break;
+                case Constants.DbDate:
+                    return DbType.Date;
+                    break;
+                default:
+                    return DbType.Unknown;
+            }
+        }
+
+    }
+}
