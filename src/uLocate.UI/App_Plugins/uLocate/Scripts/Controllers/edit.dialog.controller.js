1f231d9daf09692cf5d8e70504c9ffc80c6be514
Heather Floyd <hfloyd@mindfly.com>
2016-04-07 17:18:21 -0400

    Copying additional files which were in the KEND site, but not in uLocate.UI

---
+++ src/uLocate.UI/App_Plugins/uLocate/Scripts/Controllers/edit.dialog.controller.js
@@ -0,0 +1,15 @@
+﻿(function (controllers, undefined) {
+
+    controllers.EditDialogController = function($scope, dialogService) {
+
+        $scope.init = function() {
+            console.info('You passed in the following data: '+ $scope.dialogData.sampleItem);
+        };
+
+        $scope.init();
+    };
+
+    angular.module('umbraco').controller('uLocate.Controllers.EditDialogController', ['$scope', 'dialogService', uLocate.Controllers.EditDialogController]);
+
+
+}(window.uLocate.Controllers = window.uLocate.Controllers || {}));
---
+++ src/uLocate.UI/App_Plugins/uLocate/Scripts/Controllers/view.dialog.controller.js
@@ -0,0 +1,15 @@
+﻿(function (controllers, undefined) {
+
+    controllers.ViewDialogController = function($scope, dialogService) {
+
+        $scope.init = function() {
+            console.info('You passed in the following data: '+ $scope.dialogData.sampleItem);
+        };
+
+        $scope.init();
+    };
+
+    angular.module('umbraco').controller('uLocate.Controllers.ViewDialogController', ['$scope', 'dialogService', uLocate.Controllers.ViewDialogController]);
+
+
+}(window.uLocate.Controllers = window.uLocate.Controllers || {}));
