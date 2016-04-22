e9e06b2f4c582c2f677e329ff6d4a205d6d6cfe2
Heather Floyd <hfloyd@mindfly.com>
2016-04-22 16:53:09 -0400

    Reverting back-end Export work done (due to possible front-end solution)

--- src/uLocate/IO/Export.cs
+++ src/uLocate/IO/Export.cs
@@ -10,10 +10,9 @@
     using FileHelpers.Dynamic;
 
     using Mindfly;
-    using Newtonsoft.Json;
+
     using uLocate.Models;
     using uLocate.Persistance;
-    using Umbraco.Core.Logging;
 
     internal class Export
     {
@@ -64,105 +63,46 @@
 
             Msg.ObjectName = file;
 
-            string errorMsg = "";
-            string errorData = "";
-            int errorIndex = 0;
+            // Dynamic class definition
+            string dynamicClassDef = DynamicLocationFlat.GetDynamicClass(LocTypeKey);
+            Type DynamicLocation = ClassBuilder.ClassFromString(dynamicClassDef);
 
-            try
+            // Using http://filehelpers.sourceforge.net/ to write csv 
+            FileHelperEngine fhEngine = new FileHelperEngine(DynamicLocation);
+
+            string dynamicLocationHeaders = fhEngine.GetFileHeader(); 
+            var headers = dynamicLocationHeaders.Split(',');
+            MethodInfo setMethod = DynamicLocation.GetMethod("SetProperty");
+
+            var locationsExport = CreateListOfType(DynamicLocation);
+            //Type listType = typeof(List<>).MakeGenericType(new[] { DynamicLocation });
+            //IList locations = (IList)Activator.CreateInstance(listType);
+
+            var allLocationsOfType = Repositories.LocationRepo.GetByType(LocTypeKey);
+
+            foreach (EditableLocation loc in allLocationsOfType)
             {
-                // Dynamic class definition
-                errorIndex = 1;
-                errorMsg = "Error while creating Dynamic Class for Export. Class Definition:";
+                dynamic dynLoc = new[] { DynamicLocation };
 
-                string dynamicClassDef = DynamicLocationFlat.GetDynamicClass(LocTypeKey);
-
-                errorData = dynamicClassDef;
-                
-                Type DynamicLocation = ClassBuilder.ClassFromString(dynamicClassDef);
-
-
-                // Using http://filehelpers.sourceforge.net/ to get dynamic headers
-                errorIndex = 2;
-                errorMsg = "Error while using FileHelperEngine to get headers";
-                
-                FileHelperEngine fhEngine = new FileHelperEngine(DynamicLocation);
-                string dynamicLocationHeaders = fhEngine.GetFileHeader();
-                var headers = dynamicLocationHeaders.Split(',');
-                errorData = "dynamicLocationHeaders = " + dynamicLocationHeaders;
-
-
-                //Prepare to get a list of dynamic objects
-                errorIndex = 3;
-                errorMsg = "Error while converting Locations to dynamic objects";
-                
-                MethodInfo setMethod = DynamicLocation.GetMethod("SetProperty");
-
-                var locationsExport = CreateListOfType(DynamicLocation);
-                //Type listType = typeof(List<>).MakeGenericType(new[] { DynamicLocation });
-                //IList locations = (IList)Activator.CreateInstance(listType);
-
-
-                var allLocationsOfType = Repositories.LocationRepo.GetByType(LocTypeKey);
-
-                foreach (EditableLocation loc in allLocationsOfType)
+                foreach (string prop in headers)
                 {
-                    dynamic dynLoc = new[] { DynamicLocation };
-                    //var dynLoc = CreateType(DynamicLocation); 
-                    //dynamic dynLoc = typeof(DynamicLocation);
-                    //errorData = "dynLoc.GetType()=" + dynLoc.GetType();
-
-                    //errorData = JsonConvert.SerializeObject(dynLoc);
-                    foreach (string prop in headers)
+                    if (prop == "LocationName")
                     {
-                        var currentProperty = loc.GetType().GetProperty(prop);
-
-                        if (currentProperty != null)
-                        {
-                            Type type = DynamicLocation; //Throws System.Reflection.TargetException: 'Object does not match target type'
-                            PropertyInfo tempProperty = type.GetProperty(prop);
-                            tempProperty.SetValue(dynLoc, currentProperty.GetValue(prop));
-                        }
-
-                        if (prop == "LocationName")
-                        {
-                            object dataObj = loc.Name;
-                            dynLoc.SetProperty(prop, dataObj);
-                            //setMethod.Invoke(dynLoc, new[] { prop, dataObj });
-                        }
-                        else
-                        {
-                            var propData = loc.PropertyData.Where(n => n.PropertyAlias == prop).FirstOrDefault();
-                            //setMethod.Invoke(dynLoc, new object[] { prop, propData.Value.ValueObject });
-                            dynLoc.SetProperty(prop, propData.Value.ValueObject);
-                        }
-
+                        object dataObj = loc.Name;
+                        setMethod.Invoke(dynLoc, new object[] { prop, dataObj });
+                    }
+                    else
+                    {
+                        var propData = loc.PropertyData.Where(n => n.PropertyAlias == prop).FirstOrDefault();
+                        setMethod.Invoke(dynLoc, new object[] { prop, propData.Value.ValueObject });
                     }
 
-                    locationsExport.Add(dynLoc);
                 }
 
-                fhEngine.WriteFile(file, locationsExport.ToArray());
-
+                locationsExport.Add(dynLoc);
             }
-            catch (Exception exDynamicClass)
-            {
-                string msg = "";
 
-                switch (errorIndex)
-                {
-                    //case 1:
-                    //    msg = string.Format("Error while creating Dynamic Class for Export. Class Definition:{0}{1}", Environment.NewLine, errorData);
-                    //    break;
-                    case 2:
-                        msg = string.Format("Error while using FileHelperEngine:{0}{1}", Environment.NewLine, errorData);
-                        break;
-                    default:
-                        msg = string.Format("{0}{1}{2}",errorMsg, Environment.NewLine, errorData);
-                        break;
-                }
-
-                LogHelper.Error<Export>(msg, exDynamicClass);
-            }
+            fhEngine.WriteFile(file, locationsExport.ToArray());  
 
             return Msg;
         }
@@ -172,10 +112,6 @@
             return new List<T>();
         }
 
-        //static T CreateObjectOfType<T>(T obj)
-        //{
-        //    return <T>();
-        //}
     }
 }
 
